8b12a6a3e5ddca6cd3764ca56390c5e24dc35b84
Lars Hartvigsen <lars.hartvigsen@nav.no>
2023-11-01 14:40:57 +0100

    Disse kan være records

--- src/main/java/no/nav/okosynk/hentbatchoppgaver/lagoppgave/OsMeldingFunksjonelleAggregeringsKriterier.java
+++ src/main/java/no/nav/okosynk/hentbatchoppgaver/lagoppgave/OsMeldingFunksjonelleAggregeringsKriterier.java
@@ -3,39 +3,16 @@ package no.nav.okosynk.hentbatchoppgaver.lagoppgave;
 import no.nav.okosynk.hentbatchoppgaver.model.OsMelding;
 import no.nav.okosynk.model.GjelderIdType;
 
-public class OsMeldingFunksjonelleAggregeringsKriterier {
-
-    public final String faggruppe;
-    public final String gjelderId;
-    public final GjelderIdType gjelderIdType;
-    public final String ansvarligEnhetId;
+public record OsMeldingFunksjonelleAggregeringsKriterier(
+        String faggruppe,
+        String gjelderId,
+        GjelderIdType gjelderIdType,
+        String ansvarligEnhetId) {
 
     public OsMeldingFunksjonelleAggregeringsKriterier(OsMelding osMelding) {
-        OsMappingRegelRepository osMappingRegelRepository = new OsMappingRegelRepository();
-        this.gjelderId = osMelding.getGjelderId();
-        this.gjelderIdType = GjelderIdType.fra(osMelding.getGjelderId());
-        this.ansvarligEnhetId = osMappingRegelRepository.finnRegel(osMelding).map(t -> t.ansvarligEnhetId).orElse(null);
-        this.faggruppe = osMelding.getFaggruppe();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        OsMeldingFunksjonelleAggregeringsKriterier that = (OsMeldingFunksjonelleAggregeringsKriterier) o;
-        if (!gjelderId.equals(that.gjelderId)) return false;
-        if (gjelderIdType != that.gjelderIdType) return false;
-        if (!ansvarligEnhetId.equals(that.ansvarligEnhetId)) return false;
-        return faggruppe.equals(that.faggruppe);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = gjelderId.hashCode();
-        result = 31 * result + gjelderIdType.hashCode();
-        result = 31 * result + ansvarligEnhetId.hashCode();
-        result = 31 * result + faggruppe.hashCode();
-        return result;
+        this(osMelding.getFaggruppe(),
+                osMelding.getGjelderId(),
+                GjelderIdType.fra(osMelding.getGjelderId()),
+                new OsMappingRegelRepository().finnRegel(osMelding).map(t -> t.ansvarligEnhetId).orElse(null));
     }
 }
--- src/main/java/no/nav/okosynk/hentbatchoppgaver/lagoppgave/UrMeldingFunksjonelleAggregeringsKriterier.java
+++ src/main/java/no/nav/okosynk/hentbatchoppgaver/lagoppgave/UrMeldingFunksjonelleAggregeringsKriterier.java
@@ -3,39 +3,16 @@ package no.nav.okosynk.hentbatchoppgaver.lagoppgave;
 import no.nav.okosynk.hentbatchoppgaver.model.UrMelding;
 import no.nav.okosynk.model.GjelderIdType;
 
-public class UrMeldingFunksjonelleAggregeringsKriterier {
-
-    public final String gjelderId;
-    public final String gjelderIdType;
-    public final String ansvarligEnhetId;
-    public final String oppdragsKode;
-
+public record UrMeldingFunksjonelleAggregeringsKriterier(
+        String gjelderId,
+        String gjelderIdType,
+        String ansvarligEnhetId,
+        String oppdragsKode
+) {
     public UrMeldingFunksjonelleAggregeringsKriterier(UrMelding urMelding) {
-        UrMappingRegelRepository urMappingRegelRepository = new UrMappingRegelRepository();
-        this.gjelderId = urMelding.getGjelderId();
-        this.gjelderIdType = GjelderIdType.fra(urMelding.getGjelderId()).toString();
-        this.ansvarligEnhetId = urMappingRegelRepository.finnRegel(urMelding).map(t -> t.ansvarligEnhetId).orElse(null);
-        this.oppdragsKode = urMelding.oppdragsKode;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        UrMeldingFunksjonelleAggregeringsKriterier that = (UrMeldingFunksjonelleAggregeringsKriterier) o;
-        if (!gjelderId.equals(that.gjelderId)) return false;
-        if (!gjelderIdType.equals(that.gjelderIdType)) return false;
-        if (!ansvarligEnhetId.equals(that.ansvarligEnhetId)) return false;
-        return oppdragsKode.equals(that.oppdragsKode);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = gjelderId.hashCode();
-        result = 31 * result + gjelderIdType.hashCode();
-        result = 31 * result + ansvarligEnhetId.hashCode();
-        result = 31 * result + oppdragsKode.hashCode();
-        return result;
+        this(urMelding.getGjelderId(),
+                GjelderIdType.fra(urMelding.getGjelderId()).toString(),
+                new UrMappingRegelRepository().finnRegel(urMelding).map(t -> t.ansvarligEnhetId).orElse(null),
+                urMelding.oppdragsKode);
     }
 }
